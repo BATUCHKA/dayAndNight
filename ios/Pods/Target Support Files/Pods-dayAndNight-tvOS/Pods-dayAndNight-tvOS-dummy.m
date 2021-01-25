028a09c718356ab103b8629352cb664d55a832ed
Bataa Boldbaatar <bataaboldbaatar@Bataas-MacBook-Pro.local>
2021-01-25 16:53:59 +0800

    first push

---
+++ ios/Pods/Target Support Files/Pods-dayAndNight-tvOS/Pods-dayAndNight-tvOS-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_dayAndNight_tvOS : NSObject
+@end
+@implementation PodsDummy_Pods_dayAndNight_tvOS
+@end
---
+++ ios/Pods/Target Support Files/Pods-dayAndNight/Pods-dayAndNight-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_dayAndNight : NSObject
+@end
+@implementation PodsDummy_Pods_dayAndNight
+@end
